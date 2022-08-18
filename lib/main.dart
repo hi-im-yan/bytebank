96bb468962ec9b04add18c7c9ab522013e546e06
Yan Sergio Yudi Ajiki <yan.ajiki@mjv.com.br>
2022-08-18 14:11:20 -0400

    feat - utilizando imagens salvas em assets e modelando nova tela do app

--- lib/main.dart
+++ lib/main.dart
@@ -21,15 +21,57 @@ class BytebankApp extends StatelessWidget {
           ),
         ),
       ),
-      home: ListaTransferencia(),
+      home: const Dashboard(),
     );
   }
 }
 
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
 
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.jpeg'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 100,
+              width: 150,
+              color: Colors.orangeAccent,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  Text(
+                    'Contatos',
+                    style: TextStyle(color: Colors.white,fontSize: 16.0),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
 
-
-
-
-
+// home: ListaTransferencia(),
