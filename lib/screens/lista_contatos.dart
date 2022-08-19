5ede6308e8c8522a1a99d7324139f3d47de7bc5b
yan ajiki <yanajiki2010@hotmail.com>
2022-08-18 21:03:25 -0400

    feat - FutureBuilder para carregamento de dados do banco, tela de carregamento

--- lib/screens/lista_contatos.dart
+++ lib/screens/lista_contatos.dart
@@ -1,3 +1,5 @@
+import 'package:bytebank/database/app_database.dart';
+import 'package:bytebank/models/contato.dart';
 import 'package:bytebank/screens/formulario_contato.dart';
 import 'package:flutter/material.dart';
 
@@ -8,31 +10,74 @@ class ListaContatos extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Contatos'),
       ),
-      body: ListView(
-        children: const [
-          Card(
-            child: ListTile(
-              title: Text(
-                'Yan',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              subtitle: Text('1000', style: TextStyle(fontSize: 16.0)),
-            ),
-          ),
-        ],
+      body: FutureBuilder<List<Contato>>(
+        future: Future.delayed(const Duration(seconds: 1)).then((value) => findAll()),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          switch(snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(),
+                    Text('Carregando')
+                  ],
+                ),
+              );
+
+            case ConnectionState.active:
+              break;
+
+            case ConnectionState.done:
+              List<Contato> contatos = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  final Contato contato = contatos[index];
+                  return _ItemContato(contato: contato);
+                },
+                itemCount: contatos.length,
+              );
+          }
+          return const Text('Erro desconhecido durante carregamento de contatos');
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context)
               .push(
             MaterialPageRoute(builder: (context) => FormularioContato()),
-          ).then((contato) {
+          )
+              .then((contato) {
             if (contato != null) {
               debugPrint(contato.toString());
             }
           });
         },
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ItemContato extends StatelessWidget {
+  final Contato contato;
+
+  const _ItemContato({super.key, required this.contato});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contato.nome,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(contato.numeroConta.toString(),
+            style: TextStyle(fontSize: 16.0)),
       ),
     );
   }
